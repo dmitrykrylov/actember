e007047548741d9399ff9f524fa01e88eda03ebd
dkrylov <dimakrylov@spark-mail.ru>
2017-09-13 14:34:39 +0300

    Fix webpack warnings

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,14 @@ module.exports = {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
           'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
-          'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false',
+          {
+            loader: 'image-webpack-loader',
+            query: {
+              optipng: {
+                optimizationLevel: 4,
+              },
+            },
+          },
         ],
       },
     ],
