b63961c1c57debfcfdb67de789bb3455917193d3
dkrylov <dimakrylov@spark-mail.ru>
2017-11-04 18:36:19 +0300

    Fix base api url

--- frontend/config.js
+++ frontend/config.js
@@ -1,13 +1,12 @@
 const apiUrl = function () {
   let url = '';
 
-  if (process.env.NODE_ENV === 'local') {
-    url = '';
-  } else if (process.env.NODE_ENV === 'development') {
-    url = 'http://localhost:8000';
-  } else if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === 'production') {
     url = 'actember.herokuapp.com';
+  } else {
+    url = 'http://localhost:8000';
   }
+
   return url;
 };
 
