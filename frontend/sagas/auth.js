125effc0773badf9a2ea5da17f5a60ef58048e91
dkrylov <dimakrylov@spark-mail.ru>
2017-11-05 12:56:47 +0300

    Fix auth sagas

--- frontend/sagas/auth.js
+++ frontend/sagas/auth.js
@@ -1,15 +1,19 @@
 import { put, call } from 'redux-saga/effects';
-import callApi from '../utils/callApi';
-import { types } from '../ducks/auth';
 import axios from 'axios';
+import { push } from 'react-router-redux';
+import { types } from '../ducks/auth';
+import { API_BASE_URL } from '../config';
 
 
 export function* login({ payload }) {
   try {
-    console.log('SSSAAAGGGAAA')
-    const response = yield call(axios, ({ method: 'post', url: 'http://localhost:8000/api/api-token-auth/', data: payload }));
+    const response = yield call(
+      axios,
+      ({ method: 'post', url: `${API_BASE_URL}/api-token-auth/`, data: payload }),
+    );
     const { token } = response.data;
     yield put({ type: types.LOGIN_SUCCESS, payload: { token } });
+    yield put(push('/'));
     window.localStorage.setItem('token', token);
   } catch (error) {
     yield put({ type: types.LOGIN_FAILURE, error });
@@ -17,14 +21,12 @@ export function* login({ payload }) {
 }
 
 
-export function* logout({ payload }) {
+export function* logout() {
   try {
-    const response = yield callApi({ method: 'post', url: '/api/logout/', data: payload });
-    const { token } = response.data;
-    yield put({ type: types.LOGOUT_SUCCESS, payload: { token } });
     window.localStorage.removeItem('token');
+    yield put({ type: types.LOGOUT_SUCCESS });
+    yield put(push('/login'));
   } catch (error) {
     yield put({ type: types.LOGOUT_FAILURE, error });
   }
 }
-
